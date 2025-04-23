b47dfee653163e664597af46cf6688d9527c4e43
itsxareh <raytos.r.bsinfotech@gmail.com>
2025-04-23 09:34:32 +0800

    test

--- LOGIN_PROMPT/main.py
+++ LOGIN_PROMPT/main.py
@@ -39,7 +39,7 @@ def login():
 
     with st.container():
         st.markdown('<div class="main">', unsafe_allow_html=True)
-        logo = Image.open("assets/images/data_analyst.jpg")
+        logo = Image.open("assets/images/logo.png")
         st.image(logo, caption="Logo", width=100)
         st.markdown("## Welcome Back!")
         st.markdown("Please log in to continue")
